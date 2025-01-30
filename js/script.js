e996f7dad290d7f92ff81aaaf10ac6c00b56af8a
AQuietWolf <akmndljr@gmail.com>
2025-01-30 18:28:29 +0600

    interval 1s

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 /* */
-const icons = document.querySelectorAll('.image-icon i');
+const icons = document.querySelectorAll(".image-icon i");
 
 let i = 1;
 
 setInterval(() => {
-    i++;
+  i++;
 
-    const icon = document.querySelector('.image-icon .slide');
-    icon.classList.remove('slide');
+  const icon = document.querySelector(".image-icon .slide");
+  icon.classList.remove("slide");
 
-    if (i > icons.length) {
-        icons[0].classList.add('slide');
-        i = 1;
-    }
-    icon.nextElementSibling.classList.add('slide');
-}, 3000);
+  if (i > icons.length) {
+    icons[0].classList.add("slide");
+    i = 1;
+  }
+  icon.nextElementSibling.classList.add("slide");
+}, 1000);
